23768cf8ab7fc574a8b808f1cab2a0924415a560
Ouadie <o.sennoune@softyware.fr>
2024-03-27 18:05:32 +0100

    modification de l'action index pour récupérer l'objet request et renvoyer du json

--- Steven/src/Controller/ApiCameraController.php
+++ Steven/src/Controller/ApiCameraController.php
@@ -6,12 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
+use Symfony\Component\HttpFoundation\Request;
 class ApiCameraController extends AbstractController
 {
     #[Route('/api/camera', name: 'app_api_camera')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $x = "rrrr";
-        return new JsonResponse(['x' => $x]);
+        $valeur = $request->getPayload()->get('valeur');
+        return $this->json(['x' => 'ddfd', 'valeur' => $valeur]);
     }
+
+    
 }
